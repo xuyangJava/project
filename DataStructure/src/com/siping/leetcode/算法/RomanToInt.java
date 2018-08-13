e31e6f143796bee4bda7ba4e8e98901ae593a200
yangxu20180101/yangxu20180101 <1193427859@qq.com>
2018-08-13 14:36:33 +0800

    罗马转数字

--- DataStructure/src/com/siping/leetcode/算法/RomanToInt.java
+++ DataStructure/src/com/siping/leetcode/算法/RomanToInt.java
@@ -3,14 +3,41 @@ package com.siping.leetcode.算法;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Test;
+
 public class RomanToInt {
     /**
      * 罗马字母转int
      */
     public int romanToInt(String s) {
         Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put("M", 1000); // CD CM
         
-        
-        return 0;
+        char[] cs = s.toCharArray();
+        int last = 0;
+        int result = 0;
+        for (char c : cs) {
+            int v = map.get(String.valueOf(c));
+            if (last == 0) {
+                result = v;
+            } else if (last >= v) {
+                result += v;
+            } else {
+                result = result + v - 2*last;
+            }
+            last = v;
+        }
+        return result;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(romanToInt("MCMXCIV"));
     }
 }
